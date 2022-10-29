361b8665566328df093f4bbc0ba0d5a2e1e03b09
ygzgrdmr <100368794+ygzgrdmr@users.noreply.github.com>
2022-10-29 19:41:42 +0300

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -3,4 +3,6 @@
 film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 
 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)*/
 
-select 
+select * 
+from film 
+where  replacement_cost  12.99 between 12.99 and 16.99 replacement_cost != 16.99;
