cd8c6289cc3ea539763c2a34b9d29dc41ad6f850
ygzgrdmr <100368794+ygzgrdmr@users.noreply.github.com>
2022-10-29 19:29:39 +0300

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,1 +1,32 @@
+
+-- film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
 SELECT title, description FROM film;
+
+/*film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu 
+length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.*/
+SELECT * FROM film
+WHERE length > 60 AND length < 75;
+
+/*
+film tablosunda bulunan tüm sütunlardaki verileri 
+rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
+*/
+
+select*
+from film 
+where rental_rate= 0.99 and (replacement_cost=12.99 or replacement_cost= 28.99);
+
+/*
+customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
+*/
+select last_name
+from costumer
+where last_name='Mary';
+
+/*
+film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda 
+rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
+*/
+select *
+from film 
+where NOT length > 50 AND NOT (rental_rate = 2.99 OR rental_rate = 4.99);
