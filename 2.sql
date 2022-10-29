05d2f8cb17b62b84efaf51e756f16daa455829b4
ygzgrdmr <100368794+ygzgrdmr@users.noreply.github.com>
2022-10-29 19:45:25 +0300

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -5,4 +5,22 @@ küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız
 
 select * 
 from film 
-where  replacement_cost  12.99 between 12.99 and 16.99 replacement_cost != 16.99;
+where  replacement_cost  12.99 between 12.99 and 16.99 and replacement_cost != 16.99;
+
+/*
+.actor tablosunda bulunan first_name ve last_name sütunlardaki verileri
+first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
+*/
+
+select  first_name , last_name
+from actor 
+where first_name in ('Penelope', 'Nick', 'Ed');
+
+/*
+film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE
+replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
+*/
+
+select *
+from film 
+where  rental_rate in(0.99, 2.99, 4.99) and replacement_cost in (12.99, 15.99, 28.9);
