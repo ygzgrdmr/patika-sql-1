b31ac89ce421c8c49decf56477f13b7f1c7aa4da
ygzgrdmr <100368794+ygzgrdmr@users.noreply.github.com>
2022-10-29 19:39:00 +0300

    Create 2.sql

---
+++ 2.sql
@@ -0,0 +1,6 @@
+
+/*
+film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 
+küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)*/
+
+select 
