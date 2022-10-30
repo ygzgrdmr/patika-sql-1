ea1edffe8c709111f3692bf861d00bef7e38288e
ygzgrdmr <100368794+ygzgrdmr@users.noreply.github.com>
2022-10-30 17:48:21 +0300

    Update 5.sql

--- 5.sql
+++ 5.sql
@@ -1,1 +1,29 @@
 
+
+/*
+film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+*/
+SELECT title , length FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+
+/*
+film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
+*/
+
+select title, length 
+from film 
+where title like '%n'
+order by length asc 
+OFFSET 4
+LIMIT 5;
+
+/*
+customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+*/
+select last_name 
+from costumer 
+where store_id=1 
+ORDER BY last_name ASC
+limit 4;
