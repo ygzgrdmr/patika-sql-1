e968cfa962b91147c3679e06c67bb143fa6ecd7a
ygzgrdmr <100368794+ygzgrdmr@users.noreply.github.com>
2022-10-30 18:24:57 +0300

    Update 7.sql

--- 7.sql
+++ 7.sql
@@ -1,1 +1,27 @@
 
+
+
+/*film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.*/
+select rating 
+from film 
+group by rating;
+
+/*film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den 
+fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.*/
+select replacement_cost, count(*)
+from film 
+group by replacement_cost
+having  count(replacement_cost) >50;
+
+/*3. customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir? */
+select store_id, count(*)
+from costumer 
+group by store_id;
+
+/*4. city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir 
+sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.*/
+select country_id, count(city)
+from city	
+Group by country_id	
+order by count(city_id) 
+desc Limit 1 ;
